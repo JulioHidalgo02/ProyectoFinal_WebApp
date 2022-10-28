f7ce2399e3ae171daea54881548b791b4e5ad818
JulioHidalgo02 <j.hidalgoro@hotmail.com>
2022-10-27 18:12:02 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal_WebApp/Controllers/ProductosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFinal_WebApp.Controllers
+{
+    public class ProductosController : Controller
+    {
+        [HttpGet]
+        public IActionResult MostrarProductos()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MostrarProductoIndividual()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoFinal_WebApp/Js/Carrito.js
@@ -0,0 +1,1 @@
+﻿const Clickbutton = document.querySelectorAll(".button")
